9214b273fab8660ac2fa06e074f364a049b85de6
Per Persson <persquare@me.com>
2020-08-05 20:24:12 +0200

    Make this bundle depend on external BundleSupportPython.tmBundle

--- Support/lib/TextMate/__init__.py
+++
@@ -1,5 +0,0 @@
-from .tm_helpers import defaults_read, defaults_write
-from .tm_helpers import sh_escape, sh
-from .executor import Executor 
-
-from .webpreview import html_header, html_footer
--- Support/lib/TextMate/markdown.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-markdown = os.environ.get('TM_MARKDOWN', f"{os.environ[TM_SUPPORT_PATH]}/bin/Markdown.pl")
-
-print(f"{os.environ[TM_SUPPORT_PATH]}/bin/Markdown.pl")
